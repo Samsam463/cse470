0372048eab1d8d973da6db800dfad3d44efc08ec
Samsam463 <83468484+Samsam463@users.noreply.github.com>
2021-05-05 14:25:47 +0600

    Controller

---
+++ sign.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'dbConnection.php';
+ob_start();
+$name = $_POST['name'];
+$name= ucwords(strtolower($name));
+$email = $_POST['email'];
+$college = $_POST['college'];
+$password = $_POST['password'];
+$name = stripslashes($name);
+$name = addslashes($name);
+$name = ucwords(strtolower($name));
+$email = stripslashes($email);
+$email = addslashes($email);
+$college = stripslashes($college);
+$college = addslashes($college);
+
+
+$password = stripslashes($password);
+$password = addslashes($password);
+$password = md5($password);
+
+$q3=mysqli_query($con,"INSERT INTO user VALUES  ('$name','$college','$email','$password')");
+if($q3)
+{
+session_start();
+$_SESSION["email"] = $email;
+$_SESSION["name"] = $name;
+
+header("location:account.php?q=1");
+}
+else
+{
+header("location:index.php?q7=Email Already Registered!!!");
+}
+ob_end_flush();
+?>
